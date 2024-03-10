97bd5b58b6516e340b17b230bb7d8b7698d8cf47
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-03-10 22:02:26 +0200

    Update CSS & Tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,26 @@ import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      neutral: [
+        50,
+        ...[...Array(9).keys()].map(key => (key + 1) * 100),
+        950,
+      ].reduce(
+        (obj, shade) => ({
+          ...obj,
+          [shade]: `rgb(var(--color-neutral-${shade}) / <alpha-value>)`,
+        }),
+        {}
+      ),
+
+      background: 'rgb(var(--color-background))',
+      foreground: 'rgb(var(--color-foreground))',
+      card: 'rgb(var(--color-card))',
+    },
+  },
   plugins: [],
 };
 export default config;
