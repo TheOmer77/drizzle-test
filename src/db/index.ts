381bec1e455703fde4fe6f09130652c25b1b923c
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-03-10 21:56:25 +0200

    Use correct DB URL in DB client

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,5 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 
-const queryClient = postgres('postgres://postgres:adminadmin@0.0.0.0:5432/db');
+const queryClient = postgres(process.env.DB_URL!);
 export const db = drizzle(queryClient);
