034651f20a5df13a53163c6efe42cdd105373db2
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-03-10 21:56:08 +0200

    Update todo schema & add type

--- src/db/schema/todo.ts
+++ src/db/schema/todo.ts
@@ -1,7 +1,12 @@
-import { boolean, pgTable, text, uuid } from 'drizzle-orm/pg-core';
+import type { InferSelectModel } from 'drizzle-orm';
+import { boolean, pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core';
 
 export const todo = pgTable('todo', {
-  id: uuid('id').unique().primaryKey().defaultRandom(),
+  id: uuid('id').primaryKey().defaultRandom(),
   text: text('text').notNull(),
   checked: boolean('checked').notNull().default(false),
+  createdAt: timestamp('createdAt').notNull().defaultNow(),
+  updatedAt: timestamp('updatedAt').notNull().defaultNow(),
 });
+
+export type Todo = InferSelectModel<typeof todo>;
